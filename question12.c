3fd430fa8399d776aa357028d5e0a31b2dd908c5
Kiran_Codes <azizmallah200037@gmail.com>
2026-09-25 18:44:06 +0500

    Add files via upload

---
+++ question12.c
@@ -0,0 +1,18 @@
+// Check character entered by user is uppercase or lowercase
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z')
+        printf("%c is Uppercase\n", ch);
+    else if (ch >= 'a' && ch <= 'z')
+        printf("%c is Lowercase\n", ch);
+    else
+        printf("%c is not an alphabet\n", ch);
+
+    return 0;
+}
