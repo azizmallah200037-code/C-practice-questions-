4d29443bbe55283737ec0a3bd3257a02ed934568
Kiran_Codes <azizmallah200037@gmail.com>
2026-09-26 09:14:42 +0500

    Add files via upload

---
+++ question13.c
@@ -0,0 +1,13 @@
+// Print numbers from 1 to 100
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 100; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ question14.c
@@ -0,0 +1,13 @@
+// Print numbers from 10 to 1
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 10; i >= 1; i--) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
